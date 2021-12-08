6bb011027b9fa78ff46d59567c4abcdd99f73df3
haya <haya.saud.ruh.k@tuwaiq.edu.sa>
2021-12-08 13:22:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/busschedule/schedule/ScheduleDao.kt
@@ -0,0 +1,19 @@
+package com.example.busschedule.schedule
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.example.busschedule.Schedule
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ScheduleDao {
+    @Query("SELECT * FROM schedule ORDER BY arrival_time ASC")
+    fun getAll(): Flow<List<Schedule>>
+
+    @Query("SELECT * FROM schedule WHERE stop_name = :stopName ORDER BY arrival_time ASC")
+    fun getByStopName(stopName:String): Flow<List<Schedule>>
+
+
+
+
+}
